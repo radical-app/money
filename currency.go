4988b3785d3fbef82e0a85adabff65101e7753e7
thatside <harammin@gmail.com>
2020-04-13 22:26:40 +0300

    Improve error message for cases when currency is unknown

--- currency.go
+++ currency.go
@@ -1,7 +1,7 @@
 package money
 
 import (
-	"errors"
+	"fmt"
 	"math"
 	"strings"
 )
@@ -15,8 +15,8 @@ func CurrencyByISOCode(code string) (currency Currency, err error) {
 		return c, nil
 	}
 
-	return currency, errors.New("Currency not found")
-}
+	return currency, fmt.Errorf("currency not found: code %s", code)
+}cu
 
 // GetCurrencyByCode gets the currency object by currency ISO code
 func MustGetCurrencyByISOCode(code string) (currency Currency) {
